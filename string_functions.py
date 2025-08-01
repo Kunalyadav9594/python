9e8ab6c47248c7fe3482989eb49e44b00fdc655d
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-01 07:32:34 +0530

    casefold() function

--- string_functions.py
+++ string_functions.py
@@ -9,3 +9,9 @@ txt = "Hello, World!"
 x = len(txt)
 print(x)  # Output: 13
 # The len() function returns the length of a string.(number of characters in the string)
+
+# casefold() method
+txt = "Hello, And Welcome To My World"
+x = txt.casefold()
+print(x)  # Output: "hello, and welcome to my world"
+# The casefold() method returns a string where all characters are lower case. It is similar to the lower() method but more aggressive in its conversion.
