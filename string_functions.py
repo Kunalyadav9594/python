fde6ff0072250f3509f60fb05a26685592e58ba7
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-01 15:31:46 +0530

    center() function

--- string_functions.py
+++ string_functions.py
@@ -14,4 +14,18 @@ print(x)  # Output: 13
 txt = "Hello, And Welcome To My World"
 x = txt.casefold()
 print(x)  # Output: "hello, and welcome to my world"
-# The casefold() method returns a string where all characters are lower case. It is similar to the lower() method but more aggressive in its conversion.
+# The casefold() method returns a string where all characters are lower case. It is similar to the lower() 
+# method but more aggressive in its conversion.
+
+# center() method
+txt = "Hello, World!"
+x = txt.center(20)
+print(x)  # Output: "     Hello, World!     "
+# The center() method returns a string centered in a field of a given width. It pads the string with spaces 
+# on both sides to achieve the desired width.
+
+# parameterized center() method
+txt = "Hello, World!"
+x = txt.center(20, '*')
+print(x)  # Output: "*****Hello, World!*****"
+# The center() method can also take a second argument to specify a character to pad the string with.
