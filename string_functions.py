5743e493a51e229a8109aae5a49f7812ed98415e
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-01 06:56:21 +0530

    String functions

---
+++ string_functions.py
@@ -0,0 +1,4 @@
+txt = "hello, and welcome to my world"
+x = txt.capitalize()
+print(x)  # Output: "Hello, and welcome to my world"
+# The capitalize() method returns a string where the first character is upper case, and the rest is lower case.
