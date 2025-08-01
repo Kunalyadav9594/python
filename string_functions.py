f047e5ff6dc0dede98d258406f815ce29bd61f77
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-01 07:24:04 +0530

    len() function

--- string_functions.py
+++ string_functions.py
@@ -1,4 +1,11 @@
+# Python string capitalize() method
 txt = "hello, and welcome to my world"
 x = txt.capitalize()
 print(x)  # Output: "Hello, and welcome to my world"
 # The capitalize() method returns a string where the first character is upper case, and the rest is lower case.
+
+# len() function
+txt = "Hello, World!"
+x = len(txt)
+print(x)  # Output: 13
+# The len() function returns the length of a string.(number of characters in the string)
