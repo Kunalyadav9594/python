1b2fb5b474076b5b75903f8b126f1b949b51ede8
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-03 18:06:43 +0530

    format() function

--- string_functions.py
+++ string_functions.py
@@ -161,6 +161,10 @@ print(txt.format(x))
 txt = "The price is {:f} dollars."
 print(txt.format(x))
 
+#Use "o" to convert the number into octal format:
+txt = "The octal version of {0} is {0:o}"
+print(txt.format(10))
+
 
 
 
