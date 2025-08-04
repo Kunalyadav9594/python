b321bcbcb3a867c251072c6cc0fafe1f7ca18515
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-04 20:39:04 +0530

    create list

---
+++ list_and_tuple/createlist.py
@@ -0,0 +1,15 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+# Output: ['apple', 'banana', 'cherry']
+
+# list methods
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")  # Adds an item to the end of the list
+print(fruits)
+# Output: ['apple', 'banana', 'cherry', 'orange']
+
+a = ["apple", "banana", "cherry"]
+b = ["ford", "bmw", "volvo"]
+a.append(b)  # Appends the list b to the end of list a
+print(a)
+# Output: ['apple', 'banana', 'cherry', ['ford', 'bmw', 'volvo']]
