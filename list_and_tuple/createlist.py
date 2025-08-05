ea64c1d9f96fbfce262c4d66354d038cd98874fe
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-05 11:10:54 +0530

    pop() method

--- list_and_tuple/createlist.py
+++ list_and_tuple/createlist.py
@@ -18,3 +18,53 @@ print(a)
 fruits = ["apple", "banana", "cherry"]
 fruits.clear()  # Removes all items from the list
 print(fruits)
+
+# copying a list
+fruits = ["apple", "banana", "cherry"]
+newlist = fruits.copy()
+print(newlist)
+# Output: ['apple', 'banana', 'cherry']
+# counting items in a list
+fruits = ["apple", "banana", "cherry"]
+x= fruits.count("apple")
+print(x)
+# Output: 1
+# Return the number of items the value 9 appears in the list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9]
+x = numbers.count(9)
+print(x)
+# Output: 2
+# extending a list
+fruits = ["apple", "banana", "cherry"]
+cars = ["ford", "bmw", "volvo"]
+fruits.extend(cars)
+print(fruits)
+# Output: ['apple', 'banana', 'cherry', 'ford', 'bmw', 'volvo']
+# indexing a list
+fruits = ["apple", "banana", "cherry"]
+x= fruits.index("banana")  # Returns the index of the first occurrence of "banana"
+print(x)
+# Output: 1
+# what is the position of value 32:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 32]
+x = numbers.index(32)  # Returns the index of the first occurrence of 32
+print(x)
+# find the position of 'cherry' ,but start the search at position 4:
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "cherry"]
+x = fruits.index("cherry", 4)   # Returns the index of "cherry" starting from index 4
+print(x)
+# inserting into a list
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "orange")  # Inserts "orange" at index 1
+print(fruits)
+# Output: ['apple', 'orange', 'banana', 'cherry']
+# pop an item from a list
+fruits = ["apple", "banana", "cherry"]
+x = fruits.pop(1)
+print(x)  # Removes and returns the item at index 1
+# Output: banana
+print(fruits)  # Remaining items in the list
+# Output: ['apple', 'cherry']
+fruits.pop()  # Removes and returns the last item in the list
+print(fruits)
+# Output: ['apple'] 
