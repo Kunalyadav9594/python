41dccb4f1cea9a6f542f1403b42b3c74fba9f23d
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-06 19:15:50 +0530

    len() tuple

--- tuple.py
+++ tuple.py
@@ -2,3 +2,14 @@
 thistuple=("apple", "banana", "cherry")
 print(thistuple)
 # Output: ('apple', 'banana', 'cherry')
+
+# Since tuples are indexed , they can have items with the same value
+thistuple = ("apple", "banana", "cherry", "apple")
+print(thistuple)
+# Output: ('apple', 'banana', 'cherry', 'apple')
+
+# Print the number of items in a tuple:
+thistuple = ("apple", "banana", "cherry")
+x = len(thistuple)
+print(x)
+# Output: 3
