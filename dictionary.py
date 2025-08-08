3a8488e613c0e831cd7d8628ff239ab67f9322a2
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-08 12:35:51 +0530

    Change dictionary items

--- dictionary.py
+++ dictionary.py
@@ -50,3 +50,18 @@ print(thisdict["year"])  # Accessing the value associated with the key 'year'
 # 2. Using the get() method
 print(thisdict.get("brand"))  # Accessing the value associated with the key 'brand'
 # Output: Ford
+# The list of the keys is a view of the dictionary that any changes done to the dictionary will be reflected in the keys view
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car.keys())  # Returns a view object that displays a list of all the keys in the dictionary
+# Output: dict_keys(['brand', 'model', 'year'])
+car["color"] = "red"  # Adding a new key-value pair
+print(car.keys())
+# Change the dictionary items
+car["year"] = 2020  # Changing the value of an existing key
+print(car)
+# Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020, 'color': 'red'}
+
