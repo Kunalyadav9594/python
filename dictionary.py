bf0aa24b7368ad7dd072aac304b8fa3ccaefd916
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-08 11:36:11 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,42 @@
+# Create and print a dictionary
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+# Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+print(type(thisdict))
+# Output: <class 'dict'>
+
+# Print the 'brand' value of the dictionary
+print(thisdict["brand"])  # Accessing the value associated with the key 'brand'
+# Output: Ford
+
+# Duplicate keys are not allowed in dictionaries
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020  # This will overwrite the previous 'year' key
+}
+print(thisdict)
+# Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+# Dictionary Length
+print(len(thisdict))  # Returns the number of key-value pairs in the dictionary
+# Output: 3
+# Dictionary Items - Data Types
+# Dictionary keys can be of any immutable data type (string, number, tuple)
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "features": ["air conditioning", "navigation", "sunroof"]
+}
+print(thisdict)
+# Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'features': ['air conditioning', 'navigation', 'sunroof']}
+# The dict() Constructor
+# You can create a dictionary using the dict() constructor
+thisdict = dict(brand="Ford", model="Mustang", year=1964)
+print(thisdict)
+# Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
