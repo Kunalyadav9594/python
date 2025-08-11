df0eda82231eef5894ddb2e256b018caca8870a1
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-11 14:39:01 +0530

    Duplicate values not allowed

--- sets.py
+++ sets.py
@@ -2,3 +2,8 @@
 thisset = {"apple", "banana", "cherry"}
 print(thisset)
 # Output: {'apple', 'banana', 'cherry'}
+
+# Duplicate values will be ignored:
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+# Output: {'apple', 'banana', 'cherry'}
