3a1d885b998a2f808c2e415f7abfe50a7d019ec1
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-12 18:43:11 +0530

    Join sets

--- sets.py
+++ sets.py
@@ -123,3 +123,12 @@ set3={'John', 'Elena'}
 set4={'apple', 'banana', 'cherry'}
 my_set=set1.union(set2, set3, set4)
 print(my_set)
+
+# Use | to join two operators:
+set1={'a','b','c'}
+set2={1,2,3}
+set3={'john','Elena'}
+set4={'apple','banana','cherry'}
+
+my_set=set1|set2|set3|set4
+print(my_set)
