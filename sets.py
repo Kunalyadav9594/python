ad8edc1f3fa17e30d2b4d4a6fcb636c43e9ac189
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-13 17:34:08 +0530

    - operator instead of difference method

--- sets.py
+++ sets.py
@@ -180,3 +180,9 @@ set1={'apple','banana','cherry'}
 set2={'google','microsoft','apple'}
 set3=set1.difference(set2)
 print(set3)
+
+# You can use the - operator instead of difference() method, and you will get the same result.
+set1={'apple','orange','banana'}
+set2={'apple',1,2}
+set3=set1-set2
+print(set3)
