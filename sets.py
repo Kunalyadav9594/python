5e5ee2803ae2e7721a421c2bd5ffbad2a772bc98
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-14 14:01:01 +0530

    difference() method

--- sets.py
+++ sets.py
@@ -238,6 +238,12 @@ fruits={'apple','banana', 'cherry'}
 x=fruits.copy()
 print(x)
 
+# difference() method:
+# Return a set that contains the items that only exist in set x, and not in set y:
+x={'apple','banana','cherry'}
+y={'google','microsoft','apple'}
+z=x.difference(y)
+print(z)
 
 
 
