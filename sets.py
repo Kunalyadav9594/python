d058cf240f7d5fc428b1925b2b5627231b623a3a
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-21 17:44:50 +0530

    union() method

--- sets.py
+++ sets.py
@@ -459,3 +459,10 @@ x={'apple', 'banana', 'cherry'}
 y={'google', 'microsoft', 'apple'}
 z=x.union(y)
 print(z)
+
+# Unify more than two sets:
+x={'a', 'b', 'c'}
+y={'f','d','a'}
+z={'c', 'd', 'e'}
+result=x.union(y, z)
+print(result)
