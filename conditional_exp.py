9a4fb1654204cb502a1b8252a2c3cbac0df257c0
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-22 00:24:26 +0530

    or keyword

--- conditional_exp.py
+++ conditional_exp.py
@@ -30,3 +30,40 @@ elif a == b:
     print("a and b are equal")    
 else:
     print("a is greater than b")    
+
+# You can also have an else statement without an elif statement.
+a = 200 
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# one line if statement
+a = 200
+b = 33
+if a > b: print("a is greater than b")
+
+# one line if else statement
+a=2 
+b=330
+print("A") if a > b else print("B")
+
+# You can also have multiple else statements on the single line.
+a=330   
+b=330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# And - The and keyword is a logical operator, and is used to combine conditional statements.
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+
+# Or - The or keyword is a logical operator, and is used to combine conditional statements.
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the conditions is True") 
