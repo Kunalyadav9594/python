dbffb745aefd23e76d92f27609f5e91e8132c2bd
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-21 23:40:32 +0530

    elif statement

---
+++ conditional_exp.py
@@ -0,0 +1,22 @@
+# Python If...Else Conditional Statements
+# if statements are used to execute a block of code based on a condition.
+a=33
+b=200
+if b > a:
+    print("b is greater than a")
+# In this example we used two variables, a and b, which are used as the part of the if statement to check whether b is greater than a.
+# If the condition evaluates to True, the code block inside the if statement is executed.
+# If the condition evaluates to False, the code block inside the if statement is skipped.
+
+# Indentation is important in Python. The code block inside the if statement must be indented.
+# If the code block is not indented, Python will raise an IndentationError.
+# If statement without indentation raises an error
+
+# elif - The elif keyword is python's way of saying 'if the previous conditions were not true, then try this condition'.
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
