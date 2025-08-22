93788a87a5ad4d35c35ef6ab3659e54e5d76602a
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-22 21:01:05 +0530

    return statement

--- functions.py
+++ functions.py
@@ -21,3 +21,33 @@ my_function("Emil", "Refsnes")
 def my_function(*kids):
     print("The youngest child is " + kids[2])
 my_function("Emil", "Tobias", "Linus")
+
+# Keyword arguments - You can also send arguments with the key = value syntax.
+def my_function(child3, child2, child1):
+    print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+# Arbitary keyword arguments, **kwargs - If you do not know how many keyword arguments that will be passed into your function, add two asterisks: ** before the parameter name.
+def my_function(**kid):
+    print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
+
+# Default parameter value - If we call the function without argument, it uses the default value.
+def my_function(country = "Norway"):
+    print("I am from " + country)
+my_function("Sweden")
+my_function("India")
+my_function()  # Will use the default value "Norway"
+my_function("Brazil")
+
+# Passing a list as an argument - You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
+def my_function(food):
+    for x in food:
+        print(x)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+
+# Return values - To let a function return a value, use the return statement.
+def my_function(x):
+    return 5 * x    
+print(my_function(3))
