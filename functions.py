7de3a8912e5560733939d4cbd0262c9f0580818e
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-22 19:51:32 +0530

    Number of argument

--- functions.py
+++ functions.py
@@ -5,3 +5,14 @@ def my_function():
 # Calling a function.
 my_function()
 
+# Arguments - Information can be passed into functions as arguments.
+def my_function(fname):
+    print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+# Number of arguments - A function can have one or more arguments.
+def my_function(fname, lname):
+    print(fname + " " + lname)
+my_function("Emil", "Refsnes")
