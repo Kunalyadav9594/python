a6431ead6b2ecff652fb0e2f1fb2ab65df3595a5
Kunal Yadav <kunalyadav9594@gmail.com>
2025-08-23 23:30:11 +0530

    pop() method

--- arrays.py
+++ arrays.py
@@ -11,3 +11,23 @@ cars[0] = "Toyota"
 cars = ["Ford", "Volvo", "BMW"]
 x = len(cars)
 print(x)
+
+# Looping through an array.
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+    print(x)
+
+# Adding an array item using the append() method.
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+print(cars)
+
+# Removing an array item using the remove() method.
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+print(cars)
+
+# Removing an array item using the pop() method.
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+print(cars)
